f547fc332bc0f9bbd537d2e984fc6451f3e3ccf0
LITHIRA-SASMITHA <lithirasasmitha@gmail.com>
2025-08-17 13:34:33 +0530

    backens setup

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,25 @@ const connectDB  = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 
+const app = express();
+
+//Middleware to handle CORS
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+connectDB();
+
+app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
 
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
